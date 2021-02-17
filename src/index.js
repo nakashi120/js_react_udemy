a89b9d28a695597470d77779e1d6d0956f4da232
nakashi120 <nakashi120@gmail.com>
2021-02-17 22:46:25 +0900

    udemy-4-18

---
+++ src/index.js
@@ -0,0 +1,53 @@
+/**
+ * const, letの変数宣言
+ */
+
+// // varを使って変数宣言
+//  var val1 = "var変数";
+//  console.log(val1)
+//
+//  // var変数は上書き可能
+//  val1 = "var変数を上書き"
+//  console.log(val1)
+//
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言"
+// console.log(val1)
+//
+// // letを使って変数宣言
+// let val2 = "let変数";
+// console.log(val2)
+//
+// // letは上書きが可能
+// val2 = "let変数を上書き"
+// console.log(val2)
+//
+// // val変数は再宣言不可能
+// let val2 = "let変数を再宣言"
+// console.log(val2)
+//
+// const val3 = "const変数"
+// console.log(val3)
+//
+// val3 = "const変数を上書き"
+// console.log(val3)
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "nakashi",
+//   age: 34,
+// };
+//
+// val4.name = "ayano"
+// val4.address = "Aichi"
+//
+// console.log(val4)
+
+// constで定義した配列は変更可能
+// const val5 = [
+//   'dog',
+//   'cat',
+// ];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
