c19430568811cb64b0b0787f0b7036b9fc8e0338
nakashi120 <nakashi120@gmail.com>
2021-02-17 22:53:24 +0900

    udemy-4-19

--- src/index.js
+++ src/index.js
@@ -51,3 +51,19 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+
+/**
+ * テンプレート文字列
+ */
+const name = "nakashi";
+const age = 34;
+
+// 「わたしの名前はnakashiです。年齢は34歳です。」
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。"
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`
+console.log(message2);
