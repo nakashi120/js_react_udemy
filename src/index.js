e65adcae7cce5dcb70ffffda0c885b3087066440
nakashi120 <nakashi120@gmail.com>
2021-02-19 00:16:09 +0900

    udemy-4-25 三項演算子

--- src/index.js
+++ src/index.js
@@ -191,3 +191,20 @@
 //   }
 // })
 // console.log(newNameArr)
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrue : 条件がfalse
+// const val1 = 1 > 0 ? 'true
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+//
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum)
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています。' : '許容範囲内です。';
+// }
+// console.log(checkSum(40, 50))
